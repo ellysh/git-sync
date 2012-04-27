758434f75a7d9a630b0347efb7e58b3b38accd49
Ilya Shpigor <petrsum@gmail.com>
2012-04-27 14:50:58 +0400

    Add the uso-iso repository

--- src/configure.sh
+++ src/configure.sh
@@ -3,7 +3,7 @@
 # The global constants used in several scripts
 
 PROJECTS="step-controller libserial libatom libplanar mxu11x0 planar-display \
-          planar-editor pui pui-keyboard pui-test mems-gyroscope simunit"
+          planar-editor pui pui-keyboard pui-test mems-gyroscope simunit uso-iso"
 PROJECTS_DIR="/home/ilya/sync"
 
 # This names are the same as hostnames with gitolite to sync
